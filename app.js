dd6e889eefa0a4f882f52f9defe34b0775ce4747
Khushi Sarkari <ksarkari@ex2india.com>
2020-12-16 14:55:01 +0530

    init project

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db')
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+//load config
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+const app = express();
+if (process.env.NODE_ENV === 'development')
+    app.use(morgan('dev'));
+
+//handlebar
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main', extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Routes
+app.use('/', require('./routes/index'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
